5ac92b6522779347462dbc62656bd052b329da8c
Ashish <ashish.sah1857@gmail.com>
2021-12-21 22:38:48 +0530

    stater

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()  // using dotenv you can use  values in enviroment file
+
+const express=require('express')
+const app =express()
+
+const cors=require('cors')
+const morgan=require('morgan')
+
+port=process.env.PORT;
+
+//middle ware
+
+app.use(cors()) ;
+app.use(morgan('dev'));
+
+//rout
+
+app.get('/',(req,res)=>{
+
+return res.status(200).json({
+    'status':true,
+    'message':"server run"
+})
+});
+
+
+// start server
+app.listen( port,()=>{
+    console.log("Server runing at port number"+port);
+})
